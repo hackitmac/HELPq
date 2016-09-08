113bb958d3be6cde5f9cc007dfcfdbe31199af53
Kemal Ahmed <goatandsheep@gmail.com>
2016-09-08 13:41:57 -0400

    changed true --> 1. will it work?

--- server/startup.js
+++ server/startup.js
@@ -7,15 +7,15 @@ Meteor.startup(function(){
     config.admin.password = process.env.ADMIN_PASSWORD
   }
   
-  if (process.env.GITHUB === true) {
+  if (process.env.GITHUB === 1) {
     console.log("yes")
-  } else if (process.env.GITHUB == true) {
+  } else if (process.env.GITHUB == 1) {
     console.log("ish")
   } else {
     console.log("no")
   }
   
-  if ((process.env.GITHUB === true) && process.env.GITHUB_CLIENTID && process.env.GITHUB_SECRET){
+  if ((process.env.GITHUB === 1) && process.env.GITHUB_CLIENTID && process.env.GITHUB_SECRET){
     config.github = {
       'enable': true,
       'clientId': process.env.GITHUB_CLIENTID,
